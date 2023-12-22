87580243b749d9cadd1c6910efa49757a21beb8d
Marcio Mangar <marcio.mangar@gmail.com>
2023-12-22 15:50:01 -0300

    Auto-Commit

--- src/actions/logs_action.go
+++ src/actions/logs_action.go
@@ -3,6 +3,7 @@ package actions
 import (
 	"GoGinAPI/db"
 	"GoGinAPI/dbsqlc"
+	"GoGinAPI/sqlc"
 	"context"
 	"fmt"
 
@@ -46,7 +47,19 @@ func LogsAction(c *gin.Context) LogActionResult {
 	con.Close()
 
 	result.LogCont = len(logs)
-	result.Logs = logs
+	result.Logs = convertDBLogToLog(logs)
 	return result
 
+}
+
+
+func convertDBLogToLog(logs []sqlc.Log) []LogResult {
+	logResults := make([]LogResult, 0, len(logs))
+
+	for _, log := range logs {
+		logResult := LogResult{Id: log.ID, Mensagem: log.Mensagem}
+		logResults = append(logResults, logResult)
+	}
+
+	return logResults
 }
