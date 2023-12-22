57bc8434930b1a9e3db81e0e2c37a5038ca9046a
Marcio Garcia <t.marcio.garcia@estapar.com.br>
2023-12-22 14:20:01 -0300

    Auto-Commit

--- src/actions/logs_action.go
+++ src/actions/logs_action.go
@@ -1,5 +1,27 @@
 package actions
 
-func LogsAction() {
+import (
+	"GoGinAPI/db"
+	"GoGinAPI/dbsqlc"
+	"context"
+	"fmt"
+)
+
+
+func LogsAction() []Log {
+
+	ctx := context.Background()
+	var con = db.InitDB()
+
+	queries := dbsqlc.New(con)
+	logs, err := queries.GetAllLogs(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	con.Close()
+
+
+	return nil
 
 }
