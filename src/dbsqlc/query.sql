154c6a960b1b1b5ad9fbdc041be29a3181231d07
Marcio Mangar <marcio.mangar@gmail.com>
2023-12-23 14:20:01 -0300

    Auto-Commit

--- src/dbsqlc/query.sql
+++ src/dbsqlc/query.sql
@@ -8,3 +8,20 @@ WHERE id = (SELECT MAX(id) FROM Logs);
 -- name: InsertLog :exec
 INSERT INTO Logs ( mensagem )
 VALUES ($1);
+
+
+-- name: InsertAccount :exec
+INSERT INTO Accounts ( name, email )
+VALUES ($1, $2)
+
+
+-- name: GetAccountByEmail :one
+SELECT * FROM Accounts
+WHERE email = $1
+LIMIT 1
+
+-- name: GetAccount :one
+SELECT * FROM Accounts
+WHERE id = $1
+LIMIT 1
+
--- src/dbsqlc/schema.sql
+++ src/dbsqlc/schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS Logs
     mensagem longtext null,
     primary key (id)
 );
+
+CREATE TABLE IF NOT EXISTS Accounts
+(
+    id int auto_increment not null,
+    name varchar(255) null,
+    email varchar(255) null,
+    primary key (id)
+);
