b401c5ddae856ae3b5d919c906f793fa7c504d12
Marcio Mangar <marcio.mangar@gmail.com>
2023-12-23 21:30:01 -0300

    Auto-Commit

--- src/util/convert.go
+++ src/util/convert.go
@@ -6,14 +6,10 @@ import (
 )
 
 func Convert(result any) (string, error) {
-
 	jsonData, err := json.Marshal(result)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	// Convertendo os dados JSON para string
 	jsonString := string(jsonData)
-
 	return jsonString, err
 }
